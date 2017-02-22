8a0665a67df7a2835047ec2eaa878f3ab26850b8
doublerob7 <doublerob7@gmail.com>
2017-02-21 21:22:44 -0800

    Removed local files, started recreated 9DOF.py

--- 9DOF.py
+++ 9DOF.py
@@ -1,5 +1,6 @@
 from L3DG20 import L3DG20_2 as L3DG20
 from LSM303 import LSM303_2 as LSM303
+from fusion.fusion import Fusion
 
 
 class Event:
@@ -8,21 +9,24 @@ class Event:
         pass
 
 
-class IMU:
+class IMU(Fusion):
 
     def __init__(self):
+        super().__init__()
         self.accel = LSM303.LSM303Accel()
         self.mag = LSM303.LSM303Mag()
         self.gyro = L3DG20.L3DG20()
 
-    def orientation_accel(self):
-        # TODO: raw orientation vector (just accelerometer)
-        return self.accel.read()
-        pass
+    def smart_update(self):
+        """Use update_nomag if magnetometer isn't configured, otherwise use update()"""
+        if self.mag.bias == (0, 0, 0):
+            self.update_nomag(self.accel.read(), self.gyro.read())
+        else:
+            self.update(self.accel.read(), self.gyro.read(), self.mag.read())
 
-    def orientation_mag(self):
-        # TODO: raw orientation vector (magnetometer)
-        pass
+    def orientation(self):
+        """Return pitch, roll, heading. Note heading is not accurate if magnetometer isn't calibrated"""
+        return self.pitch, self.roll, self.heading
 
     def gravity(self):
         # TODO: output the gravity vector orientation result of sensor fusion
