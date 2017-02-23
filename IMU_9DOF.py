a906f9f9c46efb0a4e6bb4eccceaeed28055d546
doublerob7 <doublerob7@gmail.com>
2017-02-22 16:21:50 -0800

    updated IMU testing

--- IMU_9DOF.py
+++ IMU_9DOF.py
@@ -17,6 +17,8 @@ class IMU(Fusion):
         self.mag = LSM303.Magnetometer()
         self.gyro = L3DG20.Gyroscope()
 
+        self.testing = False
+
     def smart_update(self):
         """Use update_nomag if magnetometer isn't configured, otherwise use update()"""
         if self.mag.bias == (0, 0, 0):
@@ -37,5 +39,38 @@ class IMU(Fusion):
         # TODO: output the linear acceleration component of motion (translational acceleration)
         # a = F/m
         pass
+
+
+if __name__ == "__main__":
+
+    def vec_sum(*args):
+        from math import sqrt
+        return sqrt(sum((arg ** 2 for arg in args)))
+
+    print("Testing configuration. Please rotate IMU about each axis individually.")
+
+    imu = IMU()
+    imu.testing = True
+
+    while imu.testing:
+        vals = imu.accel.read()
+        accel_sum = vec_sum(vals)
+        accelx_norm, accely_norm, accelz_norm = (val/accel_sum for val in vals)
+        print("Acc x:{:<7.1f} Acc y:{:<7.1f} Acc z:{:<7.1f}".format(accelx_norm, accely_norm, accelz_norm))
+
+        vals = imu.mag.read()
+        mag_sum = vec_sum(vals)
+        magx_norm, magy_norm, magz_norm = (val / mag_sum for val in vals)
+        print("Mag x:{:<7.1f} Mag y:{:<7.1f} Mag z:{:<7.1f}".format(magx_norm, magy_norm, magz_norm))
+
+        vals = imu.gyro.read()
+        gyro_sum = vec_sum(vals)
+        gyrox_norm, gyroy_norm, gyroz_norm = (val / gyro_sum for val in vals)
+        print("Gyr x:{:<7.1f} Gyr y:{:<7.1f} Gyr z:{:<7.1f}".format(gyrox_norm, gyroy_norm, gyroz_norm))
+
+        
+
+
+
 
 
